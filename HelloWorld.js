9cf8245143449ba4e9bed6a98840777b5a15c271
Aviral Mishra <mishraaviral2000@gmail.com>
2025-04-10 22:11:17 +0530

    fs module in nodejs

---
+++ HelloWorld.js
@@ -0,0 +1,3 @@
+const sum=require('./Sum')
+console.log('hello world')
+console.log(sum(2,5))
---
+++ index.js
@@ -0,0 +1,32 @@
+const fs =require('fs')
+ 
+//sync operation
+// fs.writeFileSync('./sample.txt',"Hello Aviral")
+// fs.writeFileSync('./sample.txt'," kaise ho",{flag:"a"})
+
+//async operation mei callback function dena hota hai jo error ko handle karte hai
+// fs.writeFile('./sample.txt',"Hey Guys",(error)=>{
+//     if(error){
+//         console.log(error)
+//     }
+//     else{
+//         console.log("file written")
+//     }
+// })
+//ab yaha se comment hatakar dekh sakte ho code kaise run hota hai sync aur async
+// console.log(1)
+// const result=fs.readFileSync('./sample.txt','utf-8')
+// console.log(result)
+// console.log(2)
+
+// fs.readFile('./sample.txt','utf-8',(error,result)=>{
+//     if(error){
+//         console.log(error)
+//     }
+//     else{
+//         console.log(result)
+//     }
+// })
+// console.log(3)
+
+// //jab tak necessary na ho tab tak hamko async operation hi use karne hai
