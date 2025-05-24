6e8d1e2fa61a48c8c46a5bf64462f0595b435f16
Aviral Mishra <mishraaviral2000@gmail.com>
2025-05-24 18:48:47 +0530

    added express server

---
+++ endex.js
@@ -0,0 +1,27 @@
+//api=api is appllication programming interface that defines set of functions 
+//that programmers can use to send requests and receive responses using
+//http protocol methoda such as get and post
+
+// //imported express
+// const express = require('express') 
+
+
+// //now we will call  express and import it in variable
+// let app=express()
+
+
+
+// let port=4000
+
+// //now we listen this like we listen in node
+// app.listen(port, ()=>{
+//     console.log(`server is running at ${port}`)
+// })
+//now we created server with express in above code
+
+
+
+
+
+
+
